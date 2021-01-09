f2aecc2971527e897487c3bc16ef887d71a1438d
Pawel L <virtuozoo@o2.pl>
2021-01-09 17:26:50 +0100

    finish styles for List component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -6,15 +6,26 @@ const List = () => (
     <h2 className={styles.listTitle}>People</h2>
     <ul className={styles.list}>
       <li className={styles.listItem}>Age 40+</li>
-      <li className={styles.listItem}>
-        Ethnicity
-        <ul>
-          <li>Black</li>
-          <li>Hispanic</li>
-          <button>+</button>
+      <li className={styles.listItemExtended}>
+        <span className={styles.listItemExtendedHead}>
+          Ethnicity
+          <button className={styles.subListItemBtn}>&#x02013;</button>
+        </span>
+
+        <ul className={styles.subList}>
+          <li className={styles.subListItem}>
+            Black
+            <button className={styles.subListItemBtn}>&#x02013;</button>
+          </li>
+          <li className={styles.subListItem}>
+            Hispanic
+            <button className={styles.subListItemBtn}>&#x02013;</button>
+          </li>
+          <button className={styles.subListBtn}>&#x0002B;</button>
         </ul>
       </li>
       <li className={styles.listItem}>Incomeyearly 45k USD+</li>
+      <button className={styles.listBtn}>&#x0002B;</button>
     </ul>
   </div>
 );
